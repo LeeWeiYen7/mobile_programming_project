928fc1c858344014e4421c3b237bee0d6d8f4d90
LeeWeiYen7 <godwind721@gmail.com>
2022-02-12 00:03:46 +0800

    Update

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:final_project/constants.dart';
+import 'package:final_project/home/home-screen.dart';
+import 'package:final_project/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Claypot House',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: ksecondaryColor),
+          bodyText2: TextStyle(color: ksecondaryColor),
+        ),
+        
+      ),
+      home: const WelcomeScreen(),
+    );
+  } 
+}
